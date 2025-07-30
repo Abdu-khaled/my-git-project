3971158de019cccc50dcc6fa3a0c4deaab205c88
Abdu-khaled <abdelrahmankhalid27@gmail.com>
2025-07-30 13:20:28 +0300

    feat: Add multiplty function

--- calculator.py
+++ calculator.py
@@ -5,5 +5,11 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+
+print(f"Multiplying 6 and 7: {multiply(6, 7)}")
 print(f"Adding 5 and 3: {add(5, 3)}")
 print(f"Subtracting 10 and 4: {subtract(10, 4)}")
+
